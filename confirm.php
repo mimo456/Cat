137b380843ee112deca99273a2f926b7fc09c073
mimo <tisikinohebi@gmail.com>
2019-08-11 11:31:09 +0900

    送信ボタンの表示、非表示条件

--- confirm.php
+++ confirm.php
@@ -19,40 +19,50 @@
     $userMailAddress=$_POST['e-mail'];
     $title=$_POST['title'];
     $content=$_POST['content'];
+    $errorCheck=0;
 
     if(empty($userName)){
         print('<span class="err">お名前を入力してください</span>');
+        $errorCheck++;
     }else{
         echo htmlspecialchars('ふりがな：'.$userNamePhonetic,ENT_QUOTES);
     }
     echo('<br />');
     if(empty($userNamePhonetic)){
         print('<span class="err">ふりがなを入力してください</span>');
+        $errorCheck++;
     }else{
         echo htmlspecialchars('お名前:'.$userName,ENT_QUOTES);
     }
     echo('<br />');
     if(empty($userMailAddress)){
         print('<span class="err">メールアドレスを入力してください</span>');
+        $errorCheck++;
     }else{
-        echo htmlspecialchars('メールアドレス'.$userMailAddress.ENT_QUOTES);
+        echo htmlspecialchars('メールアドレス'.$userMailAddress,ENT_QUOTES);
     }
     echo('<br />');
     if(empty($title)){
         print('<span class="err">メッセージを入力してください</span>');
+        $errorCheck++;
     }else{
         echo htmlspecialchars('題名'.$title,ENT_QUOTES);
     }
     echo('<br />');
     if(empty($content)){
-        print('<span class="err">を入力してください</span>');
+        echo('<span class="err">を入力してください</span>');
+        $errorCheck++;
     }else{
-        echo htmlspecialchars('メッセージ本文'.$content.ENT_QUOTES);
+        echo htmlspecialchars('メッセージ本文'.$content,ENT_QUOTES);
     }
     echo('<br />');
 ?>
 <form action="Done.php" method="POST">
-    <input type="submit" value="送信します">
+    <?php
+        if($errorCheck===0){
+            echo '<input type="submit" value="送信">';
+        }
+    ?>
 </form>
 </body>
 </html>
